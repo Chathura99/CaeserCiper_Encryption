7513633fbba06d06b35a5593dd4e183708607845
Chathura <62170043+Chathura99@users.noreply.github.com>
2021-08-15 17:49:07 +0530

    Update 19001606_caseClass.scala

--- 19001606_caseClass.scala
+++ 19001606_caseClass.scala
@@ -1,1 +1,18 @@
-
+object points extends App{
+  val p1=Point(3,5)
+  val p2=Point(1,2)
+  println("First point :"+p1+"\nSecond point :"+p2+"\n")
+  
+  println("Inverse of first point :"+ p1.invert)
+  println("Sum of two points :"+ (p1+p2))
+  println("Move first points by 1 and 3 :"+ p1.move(1,3))
+  println("Distance between these two points :"+ p1.distance(p2))
+ }
+case class Point(a:Int,b:Int){
+  def x:Int=a
+  def y:Int=b
+  def +(p:Point)=Point(this.x+p.x,this.y+p.y)
+  def move(dx:Int,dy:Int)=Point(this.x+dx,this.y+dy)
+  def distance(p:Point)=Point(this.x-p.x,this.y-p.y)
+  def invert=Point(this.y,this.x)
+}
